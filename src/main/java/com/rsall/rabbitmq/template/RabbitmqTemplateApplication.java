4005ba6409f1d072244a4dc1c083295e920ea295
RolandSall <roland.salloum00@outlook.com>
2023-03-04 11:57:48 +0200

    chore: initial project setup

---
+++ src/main/java/com/rsall/rabbitmq/template/RabbitmqTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.rsall.rabbitmq.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsall/rabbitmq/template/RabbitmqTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsall.rabbitmq.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
